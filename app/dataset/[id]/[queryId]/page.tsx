761cba6b695315fa00d30f8ffbc3ed23c84bdc6e
xiangnuans <1352118502@qq.com>
2024-12-21 13:41:05 +0800

    feat: query detail style refactor

--- app/dataset/[id]/[queryId]/page.tsx
+++ app/dataset/[id]/[queryId]/page.tsx
@@ -4,6 +4,7 @@ import CustomCard from "@/components/CustomCard";
 import Header from "@/components/Header";
 import CollectedTable from "@/components/query/ColletedTable";
 import { sendGet } from "@/utils/networkUtils";
+import { Color } from "antd/es/color-picker";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Box, Flex, Grid, Image, Text } from "theme-ui";
@@ -62,118 +63,109 @@ const QueryDetailPage = () => {
         >
           <Box
             sx={{
-              textAlign: "left",
-              fontSize: 18,
-              fontWeight: "medium",
               display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
+              flexDirection: "column",
+              alignItems: "flex-start",
               mb: 2,
             }}
           >
-            <Text sx={{ fontSize: 18, fontWeight: "bold", mr: 2 }}>Query:</Text>
-            <Text sx={{ fontSize: 18, fontWeight: "medium", color: "text" }}>
+            <Text sx={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
+              Query:
+            </Text>
+            <Text sx={{ fontSize: 20, fontWeight: "bold", color: "#333333" }}>
               {query?.query_text}
             </Text>
           </Box>
           <Box
             sx={{
-              textAlign: "left",
-              fontSize: 18,
-              fontWeight: "medium",
               display: "flex",
               flexDirection: "row",
               alignItems: "center",
               mb: 2,
             }}
           >
-            <Text sx={{ fontSize: 18, fontWeight: "bold", mr: 2 }}>Model:</Text>
-            <Text sx={{ fontSize: 18, fontWeight: "medium", color: "text" }}>
-              {query?.model}
-            </Text>
-          </Box>
-          <Box
-            sx={{
-              textAlign: "left",
-              fontSize: 18,
-              fontWeight: "medium",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              mb: 2,
-            }}
-          >
-            <Text sx={{ fontSize: 18, fontWeight: "bold", mr: 2 }}>
-              Expend:
-            </Text>
-            <Text sx={{ fontSize: 18, fontWeight: "medium", color: "text" }}>
-              {`${query?.points_spent} points`}
-            </Text>
-          </Box>
-          <Box
-            sx={{
-              textAlign: "left",
-              fontSize: 18,
-              fontWeight: "medium",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              mb: 2,
-            }}
-          >
-            <Text sx={{ fontSize: 18, fontWeight: "bold", mr: 2 }}>Date:</Text>
-            <Text sx={{ fontSize: 18, fontWeight: "medium", color: "text" }}>
-              {query?.created_at}
-            </Text>
-          </Box>
-          <Box
-            sx={{
-              textAlign: "left",
-              display: "flex",
-              flexDirection: "row",
-              fontSize: 18,
-              fontWeight: "medium",
-              color: "black",
-              mb: 2,
-            }}
-          >
-            <Text sx={{ fontSize: 18, fontWeight: "bold", mr: 2 }}>
-              Dataset:
-            </Text>
-            <Text
-              sx={{ fontSize: 18, fontWeight: "medium", mx: 2, color: "text" }}
-            >
-              {query?.dataset?.name}
-            </Text>
-            {/* <Box
+            <Flex
               sx={{
-                border: "1px solid #333333",
-                borderRadius: "20px",
-                px: 2,
-                py: 1,
-                fontSize: 18,
-                fontWeight: "medium",
-                mr: 2,
-                color: "text",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "row",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                width: 200,
               }}
             >
+              <Text sx={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
+                Model:
+              </Text>
+              <Text sx={{ fontSize: 20, fontWeight: "bold", color: "#333333" }}>
+                {query?.model}
+              </Text>
+            </Flex>
+            <Flex
+              sx={{
+                flexDirection: "column",
+                alignItems: "flex-start",
+                width: 200,
+              }}
+            >
+              <Text sx={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
+                Expend:
+              </Text>
+              <Text sx={{ fontSize: 20, fontWeight: "bold", color: "#333333" }}>
+                {`${query?.points_spent ?? 0} points`}
+              </Text>
+            </Flex>
+            <Flex
+              sx={{
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
+            >
+              <Text sx={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
+                Dataset:
+              </Text>
+              <Flex sx={{ flexDirection: "row", alignItems: "center" }}>
+                <Text
+                  sx={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "#333333",
+                    mr: 2,
+                  }}
+                >
+                  {query?.dataset?.name}
+                </Text>
+                <Image
+                  src="/images/dataset/common.png"
+                  alt="arrow-right"
+                  width={76}
+                  height={"auto"}
+                  sx={{ ml: 2 }}
+                />
+              </Flex>
+            </Flex>
+          </Box>
+          <Flex
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              mb: 4,
+            }}
+          >
+            <Text sx={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
+              Date:
+            </Text>
+            <Flex sx={{ flexDirection: "row", alignItems: "center" }}>
               <Image
-                src={`/images/dataset/text.png`}
-                alt={query?.dataset?.data_type}
+                src="/images/icons/calender.png"
+                alt="calendar"
                 width={20}
                 height={20}
                 sx={{ mr: 2 }}
               />
-              {query?.dataset?.data_type}
-            </Box> */}
-            {/* <Text sx={{ fontSize: 18, fontWeight: "medium", color: "text" }}>
-              {query?.dataset?.created_at}
-            </Text> */}
-          </Box>
+              <Text sx={{ fontSize: 20, fontWeight: "bold", color: "#333333" }}>
+                {query?.created_at}
+              </Text>
+            </Flex>
+          </Flex>
         </Flex>
 
         <Grid
